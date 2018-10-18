c22d1f89b8de11bfed21864be193588da6e3c518
BackSpace Academy <31625808+backspace-academy@users.noreply.github.com>
2018-10-18 20:41:20 +1100

    Added comments

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-// Include the AWS X-Ray Node.js SDK and set configuration
+// Include the AWS X-Ray SDK
 var XRay = require('aws-xray-sdk');
+// Capture calls to the AWS SDK
 var AWS = XRay.captureAWS(require('aws-sdk'));
+// Capture http traffic
 var http = XRay.captureHTTPs(require('http'));
 
 var express = require('express');
 var bodyParser = require('body-parser');
 
+// Set region for AWS SDKs
 AWS.config.region = process.env.REGION
 
+// Configure sampling rules
 XRay.middleware.setSamplingRules('sampling-rules.json');
 
 var app = express();
@@ -16,19 +20,21 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({extended:false}));
 
-//Start X-ray segment
+//Start X-ray segment myfrontend
 app.use(XRay.express.openSegment('myfrontend'));
 
 app.get('/', function(req, res) {
+    // Start X-ray subsegment 'Page Render'
     XRay.captureAsyncFunc('Page Render', function(seg) {
       res.render('index', {
         title: 'BackSpace Academy and AWS X-Ray'
       });
-      seg.close();
+      seg.close(); // Close X-ray subsegment 'Page Render'
     });
     res.status(200).end();
 });
 
+//Close X-ray segment myfrontend
 app.use(XRay.express.closeSegment());
 
 var port = process.env.PORT || 3000;
